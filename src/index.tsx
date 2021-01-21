4ea5df5ae0b3c1ab8f37b02483b66fafa1beb7fd
Paul Ganster <paul@ganster.dev>
2021-01-21 17:29:07 +0100

    Add new service worker structure of CRA https://github.com/facebook/create-react-app/issues/10032

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import {Share} from "./Share";
 
 ReactDOM.render(
@@ -15,7 +15,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorkerRegistration.register();
 
 window.addEventListener('load', () => {
   // @ts-ignore
