ea03533d682be1f020e21f9b5a58365c1192f119
Paul Ganster <paul@ganster.dev>
2021-01-21 18:18:14 +0100

    Auto download shared URLs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useState, useEffect} from 'react';
 import './App.scss';
 import {Share} from "./Share";
 
@@ -22,61 +22,75 @@ interface State {
 export default function App() {
   // @ts-ignore
   const parsedUrl = new URL(window.location);
-  console.log('within App - window.location.href' + window.location.href);
-  console.log('within App - title shared: ' + parsedUrl.searchParams.get('title'));
-  console.log('within App - text shared: ' + parsedUrl.searchParams.get('text'));
-  console.log('within App - URL shared: ' + parsedUrl.searchParams.get('url'));
+  const sharedTitle = parsedUrl.searchParams.get('title');
+  const sharedText = parsedUrl.searchParams.get('text');
+  const sharedUrl = parsedUrl.searchParams.get('url');
+
+  const urlToShare: string | null = sharedUrl ?? sharedText;
 
   const [state, setState] = useState<State>({
     url: "",
     downloadedFile: null,
     error: null
   });
-
   const [loading, setLoading] = useState(false);
+
+  const share = new Share();
+
+  useEffect(() => {
+    if (urlToShare !== null) {
+      setState({...state, url: urlToShare});
+      downloadFile(urlToShare);
+    }
+  }, []);
+
+
+  function downloadFile(url: string) {
+    setLoading(true);
+    share.share(url)
+        .then(shareResponse => {
+          // for longer videos the download promise doesn't count as a "user gesture" anymore
+          // and a "DOMException: Failed to execute 'share' on 'Navigator': Must be handling a user gesture to perform a share request."
+          // is thrown, therefore we need a new click and share the file directly
+          if(shareResponse.shareError === null) {
+            return;
+          }
+          setState({...state, downloadedFile: shareResponse.file});
+        })
+        .catch(error => {
+          console.error("An error has occured during submitting", error);
+          let msg = error;
+          if(error.message) {
+            msg = error.message;
+          }
+          setState({...state, error: msg});
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+  }
 
   function handleSubmit(event: ChangeEvent<HTMLFormElement>) {
     const downloadedFile = state.downloadedFile;
     setState({...state, error: null, downloadedFile: null});
-    const share = new Share();
     if(downloadedFile !== null) {
       share.processFile(downloadedFile)
-        .then(shareResponse => {
-          if(shareResponse.shareError === null) {
-            return;
-          }
-          setState({...state, error: shareResponse.shareError});
-        });
+          .then(shareResponse => {
+            if(shareResponse.shareError === null) {
+              return;
+            }
+            setState({...state, error: shareResponse.shareError});
+          });
       return;
     }
-    setLoading(true);
-    share.share(state.url)
-      .then(shareResponse => {
-        // for longer videos the download promise doesn't count as a "user gesture" anymore
-        // and a "DOMException: Failed to execute 'share' on 'Navigator': Must be handling a user gesture to perform a share request."
-        // is thrown, therefore we need a new click and share the file directly
-        if(shareResponse.shareError === null) {
-          return;
-        }
-        setState({...state, downloadedFile: shareResponse.file});
-      })
-      .catch(error => {
-        console.error("An error has occured during submitting", error);
-        let msg = error;
-        if(error.message) {
-          msg = error.message;
-        }
-        setState({...state, error: msg});
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    downloadFile(state.url);
     event.preventDefault();
   }
 
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
     setState({...state, url: event.target.value, downloadedFile: null})
   }
+
 
   return (
     <div className="App">
@@ -88,6 +102,21 @@ export default function App() {
         <input type="text" value={state.url} onChange={handleChange}/>
         <button>{loading ? "Loading..." : (state.downloadedFile === null ? "Download & Share" : "Share")}</button>
       </form>
+      {sharedTitle &&
+      <div>
+        title: {sharedTitle}
+      </div>
+      }
+      {sharedText &&
+      <div>
+        text: {sharedText}
+      </div>
+      }
+      {sharedUrl &&
+      <div>
+        url: {sharedUrl}
+      </div>
+      }
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import {Share} from "./Share";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,41 +15,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorkerRegistration.register();
-
-window.addEventListener('load', () => {
-  // @ts-ignore
-  const parsedUrl = new URL(window.location);
-  console.log('load - window.location.href' + window.location.href);
-  console.log('load - title shared: ' + parsedUrl.searchParams.get('title'));
-  console.log('load - text shared: ' + parsedUrl.searchParams.get('text'));
-  console.log('load - URL shared: ' + parsedUrl.searchParams.get('url'));
-});
-window.addEventListener('DOMContentLoaded', () => {
-  // @ts-ignore
-  const parsedUrl = new URL(window.location);
-  console.log('DOMContentLoaded - window.location.href' + window.location.href);
-  console.log('DOMContentLoaded - title shared: ' + parsedUrl.searchParams.get('title'));
-  console.log('DOMContentLoaded - text shared: ' + parsedUrl.searchParams.get('text'));
-  console.log('DOMContentLoaded - URL shared: ' + parsedUrl.searchParams.get('url'));
-});
-
-
-
-window.addEventListener('load', () => {
-  // TODO: this doesn't seem to work??
-  // TODO: this should be in component for loading animation etc.
-  // @ts-ignore
-  const parsedUrl = new URL(window.location);
-  console.log('Title shared: ' + parsedUrl.searchParams.get('name'));
-  console.log('Text shared: ' + parsedUrl.searchParams.get('description'));
-  console.log('Link shared: ' + parsedUrl.searchParams.get('link'));
-  console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
-  /*
-  if(parsedUrl.searchParams.get('link') !== null) {
-    const share = new Share();
-    share.share(parsedUrl.searchParams.get('link'));
-  } else if(parsedUrl.searchParams.get('url') !== null) {
-    const share = new Share();
-    share.share(parsedUrl.searchParams.get('url'));
-  }*/
-});
